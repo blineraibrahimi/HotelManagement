2bfd5b885f993e41f26939fbd735095983756a2d
blineraibrahimi <iblinera@gmail.com>
2023-04-17 14:16:32 +0200

    changes

--- HotelManagement/Customers/RegisterCustomer.cs
+++ HotelManagement/Customers/RegisterCustomer.cs
@@ -19,33 +19,6 @@ namespace HotelManagement.Customers
             InitializeComponent();
         }
 
-        private void empRegisterbtn_Click(object sender, EventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(txtCusName.Text) || string.IsNullOrWhiteSpace(txtCusLastName.Text) ||
-                string.IsNullOrWhiteSpace(txtCusAddress.Text) || string.IsNullOrWhiteSpace(txtCusContactNo.Text) || string.IsNullOrWhiteSpace(txtCusIDNo.Text) ||
-                string.IsNullOrWhiteSpace(txtCusDsscription.Text))
-            {
-                MessageBox.Show("Please fill all the inputs!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-
-            var parameters = new[]
-                {
-                    new SqlParameter("@CustomerName", SqlDbType.VarChar) { Value = txtCusName.Text },
-                    new SqlParameter("@CustomerLastname", SqlDbType.VarChar) { Value = txtCusLastName.Text },
-                    new SqlParameter("@Address", SqlDbType.VarChar) { Value = txtCusAddress.Text },
-                    new SqlParameter("@ContactNo", SqlDbType.VarChar) { Value = txtCusContactNo.Text },
-                    new SqlParameter("@CustomerIDNo", SqlDbType.VarChar) { Value = txtCusIDNo.Text },
-                    new SqlParameter("@Description", SqlDbType.VarChar) { Value = txtCusDsscription.Text },
-                };
-
-            var result = DatabaseHelper.ExecuteStoredProcedure(StoredProcedures.CreateCustomer, parameters);
-
-            MessageBox.Show("Customer Registered", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-            this.Close();
-        }
-
         private void cusRegisterbtn_Click(object sender, EventArgs e)
         {
             if (string.IsNullOrWhiteSpace(txtCusName.Text) || string.IsNullOrWhiteSpace(txtCusLastName.Text) ||
